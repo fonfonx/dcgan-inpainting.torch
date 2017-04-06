8d6b1b6f47b10418ddfe336380393ad71d65a153
fonfonx <xavier.fontaine@polytechnique.org>
2017-04-06 17:41:49 -0400

    Save completed images

--- inpainting.lua
+++ inpainting.lua
@@ -1,37 +1,42 @@
 require 'image'
 require 'nn'
 require 'optim'
+require 'paths'
 
 torch.setdefaulttensortype('torch.FloatTensor')
 
 opt = {
-    batchSize = 64,
-    beta1 = 0.9,
-    dataset = 'folder',
+    batchSize = 64,         -- size of a batch of images
+    beta1 = 0.9,            -- beta param for adam
+    dataset = 'folder',     -- folder (see data/donkey_folder.lua)
     display = 2929,
+    gpu = 2,
     imgSize = 64,
-    gpu = 1,
-    lambda = 0.004,
+    lambda = 0.002,
     loadSize = 64,
-    lr = 0.03,
-    netD = 'checkpoints/celebA-soumith_9_net_D.t7',
-    netG = 'checkpoints/celebA-soumith_9_net_G.t7',
-    noise = 'normal'
-    nIter = 10000,
+    lr = 0.02,
+    name = 'celebA',
+    net = 'celebA-normal',
+    nIter = 2000,
+    noise = 'normal',
+    nz = 100,
     win_id = 1000,
 }
 
-for k,v in pairs(opt) do opt[k] = tonumber(os.getenv(k)) or os.getenv(k) or opt[k] end
+opt = xlua.envparams(opt)
 print(opt)
 
 if opt.display > 0 then
     display = require 'display'
     display.configure({ hostname = '0.0.0.0', port = opt.display })
+else
+    opt.display = false
 end
 
 if opt.gpu > 0 then
     require 'cunn'
     require 'cudnn'
+    cutorch.setDevice(opt.gpu)
 end
 
 optimConfig = {
@@ -39,28 +44,31 @@ optimConfig = {
     beta1 = opt.beta1,
 }
 
-netG = torch.load(opt.netG)
-netD = torch.load(opt.netD)
+-- load the networks
+local netD = 'checkpoints/' .. opt.net .. '_net_D.t7'
+local netG = 'checkpoints/' .. opt.net .. '_net_G.t7'
+netG = torch.load(netG)
+netD = torch.load(netD)
 
 -- local L1Criterion = nn.AbsCriterion()
 local L1Criterion = nn.SmoothL1Criterion()
 local L2Criterion = nn.MSECriterion()
 local BCECriterion = nn.BCECriterion()
 
-img = image.load('celebA/images/000008.jpg')
-
+-- get a ramdom batch of images to complete
 local DataLoader = paths.dofile('data/data.lua')
 local data = DataLoader.new(0, opt.dataset, opt)
 local images = data:getBatch()
 
+-- mask the images
 local height = images:size(3)
 local width = images:size(4)
-
 mask = torch.Tensor(images:size()):fill(1)
-mask:narrow(3, 16, 32):narrow(4, 16, 32):zero()
+mask:narrow(3, height / 4, height / 2):narrow(4, width / 4, width / 2):zero()
 
-z = torch.Tensor(images:size(1), 100, 1, 1)
-if noise == 'uniform' then
+-- sample some noise
+z = torch.Tensor(images:size(1), opt.nz, 1, 1)
+if opt.noise == 'uniform' then
     z:uniform(-1, 1)
 else
     z:normal(0, 1)
@@ -84,17 +92,13 @@ end
 
 local masked_img = torch.cmul(images, mask)
 
+-- function performing completion
 local complete = function(masked_img, mask, z)
     local gen = netG:forward(z)
     return masked_img + torch.cmul(mask:clone():fill(1) - mask, gen)
 end
 
-if display then
-    display.image(images, { win = opt.win_id, title = "original images" })
-    display.image(masked_img, { win = opt.win_id + 1, title = "masked images" })
-    display.image(complete(masked_img, mask, z), { win = opt.win_id + 2, title = "inpainted images" })
-end
-
+-- closure computing df/dz and f(z)
 local loss_dL_dz = function(z)
     mlpG = netG:clone('weight', 'bias');
     mlpD = netD:clone('weight', 'bias');
@@ -115,19 +119,41 @@ local loss_dL_dz = function(z)
     -- sum
     local err = contextual_err + opt.lambda * perceptual_err
     -- print(err, contextual_err, perceptual_err)
+
     return err, grads
 end
 
 print 'Inpainting...'
 
+local save_dir = 'completed/' .. opt.name
+paths.mkdir(save_dir)
+
+if display then
+    display.image(images, { win = opt.win_id, title = "original images" })
+    display.image(masked_img, { win = opt.win_id + 1, title = "masked images" })
+    display.image(complete(masked_img, mask, z), { win = opt.win_id + 2, title = "inpainted images" })
+end
+
+image.save(save_dir .. '/image_000.jpg', image.toDisplayTensor({ input = masked_img, nrow = 8 }))
+
 for iter = 1, opt.nIter do
-    z = optim.adam(loss_dL_dz, z, optimConfig):clamp(-1, 1)
+    z = optim.adam(loss_dL_dz, z, optimConfig)
+
+    if opt.noise == 'uniform' then z = z:clamp(-1, 1) end
+
+    -- display in browser and save images
     if iter % 20 == 0 then
         local gen = netG:forward(z)
         local masked_gen = torch.cmul(gen, mask)
         local comp_img = complete(masked_img, mask, z)
-        display.image(comp_img, { win = opt.win_id + 2, title = "inpainted images" })
-        display.image(masked_gen, { win = opt.win_id + 4, title = "masked generated images" })
-        display.image(gen, { win = opt.win_id + 3, title = "generated images" })
+        if display then
+            display.image(comp_img, { win = opt.win_id + 2, title = "inpainted images" })
+            display.image(masked_gen, { win = opt.win_id + 4, title = "masked generated images" })
+            display.image(gen, { win = opt.win_id + 3, title = "generated images" })
+        end
+
+        image.save((save_dir .. '/image_%03d.jpg'):format(iter / 20), image.toDisplayTensor({ input = comp_img, nrow = 8 }))
+
+        xlua.progress(iter / 20, opt.nIter / 20)
     end
 end
